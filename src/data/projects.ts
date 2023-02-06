4092e9049ee032ae19cca792810445fc837a867b
kaicoleridge <kai@coleridge.me>
2023-02-06 18:32:00 +0000

    init

---
+++ src/data/projects.ts
@@ -0,0 +1,13 @@
+export interface Project {
+    title: string;
+    description: string;
+    href?: string;
+}
+
+export const Projects: Project[] = [
+    {
+        title: 'Vector Discord Bot',
+        description: 'A Discord bot that allows users to create and manage their own vector graphics.',
+        href: '',
+    },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,29 @@
+export interface Social {
+    name: string;
+    href: string;
+    icon?: string;
+
+}
+
+export const SocialLinks: Social[] = [
+    {
+        name: 'GitHub',
+        href: 'https://github.com/kaicoleridge',
+    },
+
+    {
+        name: 'Twitter',
+        href: 'https://twitter.com/kaicoleridge',
+    },
+
+    {
+        name: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/kaicoleridge/',
+    },
+
+    {
+        name: 'Email',
+        href: 'mailto:kai@coleridge.dev',
+    },
+    
+];
