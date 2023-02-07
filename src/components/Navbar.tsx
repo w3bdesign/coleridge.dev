d8da7ae051dec7dc10e48648a738b6baf3a41175
kaicoleridge <kai@coleridge.me>
2023-02-07 03:15:26 +0000

    added border to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <nav className="bg-gray-900 md:rounded-md text-sm text-white font-bold md:ml-10 md:mt-5 py-4 px-6 md:px-10 md:w-[90%]">
-        <div className="space-x-7 text-gray-400 invisible lg:visible ">
+    <nav className="bg-gray-900 md:rounded-md text-sm border border-slate-800/50 text-white font-bold md:ml-10 md:mt-5 py-4 px-6 md:px-10 md:w-[90%]">
+        <div className="space-x-7 text-gray-400 invisible md:visible ">
             <Link href="/" className='text-xl visible '>./</Link>
             <Link href="/" className='hover:bg-slate-700 p-2 rounded-2xl'>/about</Link>
             <Link href="/" className='hover:bg-slate-700 p-2 rounded-2xl'>/projects</Link>
