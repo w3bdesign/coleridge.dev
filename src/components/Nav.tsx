299b3ae769f181e03f120f17b0806ccb9a131c3d
kaicoleridge <kai@coleridge.me>
2023-02-08 03:53:34 +0000

    init

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,31 +3,34 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-       <nav className="flex md:w-full justify-center">
-            <div className='flex items- rounded-full bg-slate-900 text-neutral-400 font-semibold mt-8 py-2 px-5 space-x-5 md:w-[70%]'>
-            <button className='font-bold text-lg rounded-full px-3 py-1'>
+       <nav className="w-full sticky md:top-5 z-50 bg-gray-900 font-semibold px-5 py-3 md:mt-4 container mx-auto rounded-md backdrop-blur-lg">
+            <div className='text-md text-gray-400 md:space-x-4'>
+            <button className='font-bold text-lg rounded-full px-5 py-1'>
                     <Link href='/'>
                        /.
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-full px-3 py-1'>
+                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
                     <Link href='/'>
-                        Home
+                        about
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-full px-3 py-1'>
+                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
                     <Link href='/'>
-                        About
+                        projects
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-full px-3 py-1'>
+                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
                     <Link href='/'>
-                        Contact
+                        contact
                     </Link>
                 </button>
+            
+
+
 
             </div>
        </nav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 import '@/styles/design.css'
 import type { AppProps } from 'next/app'
 
