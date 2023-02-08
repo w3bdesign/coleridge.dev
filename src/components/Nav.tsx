4f94ad9782bc99519b16e5839a8adfc2c13a096c
kaicoleridge <kai@coleridge.me>
2023-02-08 04:27:12 +0000

    minor updates

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,27 +3,27 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-       <nav className="w-full sticky md:top-5 z-50 bg-gray-900 font-semibold px-5 py-3 md:mt-4 container mx-auto rounded-md backdrop-blur-lg">
-            <div className='text-md text-gray-400 md:space-x-4'>
-            <button className='font-bold text-lg rounded-full px-5 py-1'>
+       <nav className="w-full sticky md:top-5 z-50 font-semibold px-5 py-3 md:mt-4 container mx-auto rounded-md backdrop-blur-lg">
+            <div className='text-md text-white md:space-x-4'>
+            <button className='font-extrabold text-lg rounded-full line-through px-5 py-1'>
                     <Link href='/'>
-                       /.
+                       //
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
+                <button className='hover:bg-slate-600 hover:text-white rounded-md px-3 py-1'>
                     <Link href='/'>
                         about
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
+                <button className='hover:bg-slate-600 hover:text-white rounded-md px-3 py-1'>
                     <Link href='/'>
                         projects
                     </Link>
                 </button>
 
-                <button className='hover:bg-slate-600 rounded-md px-3 py-1'>
+                <button className='hover:bg-slate-600 hover:text-white rounded-md px-3 py-1'>
                     <Link href='/'>
                         contact
                     </Link>
