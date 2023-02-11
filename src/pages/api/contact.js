5f5bb7c0dd98546f2d9a465118ac5769258dcbc6
kaicoleridge <kai@coleridge.me>
2023-02-11 03:25:47 +0000

    created api route for sendgrid

---
+++ src/pages/api/contact.js
@@ -0,0 +1,29 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    res.status(200).json({ status: 'Ok' });
+    
+
+
+    const message = `
+     Name: ${body.name}\r\n
+     Email: ${body.email}\r\n
+     Message: ${body.message}
+`;
+
+    const data = {
+        to: 'kaicoleridge13@gmail.com',
+        from: 'kai@coleridge.dev',
+        subject: 'New message from contact form',
+        text: message,
+        html: message.replace(/\r\n/g, '<br>'),
+    };
+
+    await mail.send(data);
+
+    res.status(200).json({ status: 'Ok' });
+};
