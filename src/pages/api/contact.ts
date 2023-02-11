f953f2edbc3adb461c36ad9849f982a2cb0a6a32
kaicoleridge <kai@coleridge.me>
2023-02-11 20:44:43 +0000

    fixed bug where form would clear on validation

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -3,15 +3,15 @@ const mail = require('@sendgrid/mail');
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async (req: { body: any; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { status: string; }): void; new(): any; }; }; }) => {
-    const body = req.body;
-    res.status(200).json({ status: 'Ok' });
+  const body = req.body;
+  res.status(200).json({ status: 'Ok' });
 
-    if (!body.name || !body.email || !body.message) {
-        return res.status(400).json({ status: 'Bad Request' });
-    }
+  if (!body.name || !body.email || !body.message) {
+    return res.status(400).json({ status: 'Bad Request' });
+  }
 
-    
-    const message = `
+
+  const message = `
 <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
   <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
   <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
@@ -42,15 +42,15 @@ export default async (req: { body: any; }, res: { status: (arg0: number) => { ()
      
 `;
 
-    const data = {
-        to: process.env.EMAIL_ID,
-        from: 'kai@coleridge.dev',
-        subject: 'New message from contact form',
-        text: message,
-        html: message.replace(/\r\n/g, '<br>'), 
-    };
+  const data = {
+    to: process.env.EMAIL_ID,
+    from: 'kai@coleridge.dev',
+    subject: 'New message from contact form',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  };
 
-    await mail.send(data);
+  await mail.send(data);
 
-    res.status(200).json({ status: 'Ok' });
+  res.status(200).json({ status: 'Ok' });
 };
