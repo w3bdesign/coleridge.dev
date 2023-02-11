f97f1b9e9520c3b60fb76a16c92ab2ef90d62d7b
kaicoleridge <kai@coleridge.me>
2023-02-11 04:18:43 +0000

    minor changes to api

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,7 +2,7 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async (req, res) => {
+export default async (req: { body: any; }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { status: string; }): void; new(): any; }; }; }) => {
     const body = req.body;
     console.log(body);
     res.status(200).json({ status: 'Ok' });
@@ -20,7 +20,7 @@ export default async (req, res) => {
         from: 'kai@coleridge.dev',
         subject: 'New message from contact form',
         text: message,
-        html: message.replace(/\r\n/g, '<br>'),
+        html: message.replace(/\r\n/g, '<br>'), 
     };
 
     await mail.send(data);
