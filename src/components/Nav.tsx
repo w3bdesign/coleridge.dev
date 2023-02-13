9e4bb1c985169eac4baaaa7e04b1b54c41dfedde
kaicoleridge <kai@coleridge.me>
2023-02-13 06:38:45 +0000

    init

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,40 +1,41 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+
+import React, { useEffect, useState } from "react";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
+import "react-tooltip/dist/react-tooltip.css";
 
 const Navbar = () => {
-    return (
-       <nav className="w-full sticky md:top-5 z-50  dark:bg-[#12181d]/60 font-semibold md:py-2 px-3 py-4 container mx-auto md:rounded-md backdrop-blur-lg">
-            <div className='text-sm text-slate-400 md:space-x-4'>
-            <button className='font-extrabold text-2xl rounded-full px-5 py-1'>
-                    <Link href='/'>
-                    ツ
-                    </Link>
-                </button>
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  return (
+    <nav className="w-full sticky md:top-5 z-50  dark:bg-[#12181d]/60 font-semibold md:py-2 px-3 py-4 container mx-auto md:rounded-md backdrop-blur-lg">
+      <div className="text-sm text-slate-400 md:space-x-4">
+        <button className="font-extrabold text-2xl rounded-full px-5 py-1">
+          <Link href="/">ツ</Link>
+        </button>
 
-                <button id="about" className='transition ease-in-out hover:bg-slate-600  hover:text-white rounded-md px-3 py-1 md:visible' data-tooltip-content="Coming Soon">
-                    <Link href='/'>
-                        about
-                    </Link>
-                </button>
+        <button
+          id="about"
+          className="transition ease-in-out hover:bg-slate-600  hover:text-white rounded-md px-3 py-1 md:visible"
+          data-tooltip-content="Coming Soon"
+        >
+          <Link href="/">about</Link>
+        </button>
 
-                <button className='transition ease-in-out hover:bg-slate-600 hover:text-white rounded-md px-3 py-1 md:visible'>
-                    <Link href='/projects'>
-                        projects
-                    </Link>
-                </button>
+        <button className="transition ease-in-out hover:bg-slate-600 hover:text-white rounded-md px-3 py-1 md:visible">
+          <Link href="/projects">projects</Link>
+        </button>
 
-                <button className='transition ease-in-out hover:bg-slate-600 hover:text-white rounded-md px-3 py-1 md:visible'>
-                    <Link href='/contact'>
-                       contact
-                    </Link>
-                </button>
-                
-            </div>
-           
-       </nav>
-       
-    )
-}
-
+        <button className="transition ease-in-out hover:bg-slate-600 hover:text-white rounded-md px-3 py-1 md:visible">
+          <Link href="/contact">contact</Link>
+        </button>
+      </div>
+      {mounted ? <ReactTooltip anchorId="about" /> : null}
+    </nav>
+  );
+};
 
 export default Navbar;
