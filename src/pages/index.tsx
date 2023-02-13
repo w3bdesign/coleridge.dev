0160b488179e36b71339c123a87c070fb747fdce
kaicoleridge <kai@coleridge.me>
2023-02-13 07:32:03 +0000

    changes to metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,12 @@ export default function Home() {
         <div>
             <Head>
                 <title>Kai Coleridge</title>
+                <meta charSet="utf-8" />
                 <meta name="description" content="Kai Coleridge's Portfolio" />
+                <meta name="author" content="Kai Coleridge" />
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="theme-color" content="#12181d"></meta>
+                <meta name="keywords" content="Kai Coleridge, full stack developer, web developer"></meta>
             </Head>
 
 
