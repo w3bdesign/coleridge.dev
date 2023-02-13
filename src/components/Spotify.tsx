1abe12835dfbc13084789f5bfdacea2b1ac56005
kaicoleridge <kai@coleridge.me>
2023-02-13 04:57:27 +0000

    commit

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -9,13 +9,13 @@ const Spotify = () => {
     
  
     // refresh component every minute to update the song from discord
-    /*useEffect(() => {
+    useEffect(() => {
         const interval = setInterval(() => {
             window.location.reload()
         }, 20000)
         return () => clearInterval(interval)
     }, [])
-    */
+    
     
 
 
