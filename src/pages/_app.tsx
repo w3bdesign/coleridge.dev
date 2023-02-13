d364d3fb0558ffcc52d72d59ae5f400cf0bc800a
kaicoleridge <kai@coleridge.me>
2023-02-13 05:55:42 +0000

    commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,8 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-   
     <Layout />
      <Component {...pageProps} />
-
     </>
     
  
