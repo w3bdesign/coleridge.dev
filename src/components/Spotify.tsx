2d37f54ef29a83ef2f0a01eb8af1d568b5e8b09f
kaicoleridge <kai@coleridge.me>
2023-02-15 15:44:05 +0000

    minor ui changes

--- src/components/Spotify.tsx
+++ src/components/Spotify.tsx
@@ -49,7 +49,7 @@ const Spotify = () => {
         </span>
       </p>
 
-      <div className="text-green-500 text-lg font-semibold mt-5 pr-3 p-2 flex items-center dark:bg-wh/60 dark:bg-[#12181d]/60 border-4 rounded-md overflow-hidden border-green-400">
+      <div className="text-green-500 text-lg font-semibold mt-5 pr-3 p-2 flex items-center dark:bg-wh/60 dark:bg-[#12181d]/60 border-4 rounded-lg overflow-hidden border-green-400">
         <img
           src={data.spotify.album_art_url ?? ""}
           className="w-[10rem] h-[6rem] md:h-[8rem] mr-5 pointer-events-none"
