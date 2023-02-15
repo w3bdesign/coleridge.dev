f3cdcea74d0cccd671da7a0ced4d002ef185cdd0
kaicoleridge <kai@coleridge.me>
2023-02-15 00:16:59 +0000

    commit

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="md:w-[60rem] sticky md:top-5 z-50 dark:bg-[#12181d]/60 font-bold md:py-2 md:mt-5 py-3 container mx-auto md:rounded-lg backdrop-blur-md">
       <div className="text-sm text-slate-400 md:space-x-4">
-        <button className="font-extrabold text-2xl rounded-full px-4 py-1">
+        <button className="font-extrabold text-2xl rounded-full px-6 py-1">
           <Link href="/">ツ</Link>
         </button>
 
