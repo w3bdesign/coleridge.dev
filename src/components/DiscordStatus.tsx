68f501c6b8dc4e7312992ed0f20bcc34df6d9abb
kaicoleridge <kai@coleridge.me>
2023-02-19 17:21:54 +0000

    mistyped tailwind prop

--- src/components/DiscordStatus.tsx
+++ src/components/DiscordStatus.tsx
@@ -4,9 +4,10 @@ const DISCORD_ID = "328282806327181322";
 
 const DiscordStatus = () => {
   const data  = useLanyardWS(DISCORD_ID);
+  
   if (data?.discord_status === "dnd") {
     return (
-      <div className="flex flex-col items-start justify-center textxl font-semibold px-4 pl-3 pr-20 py-4 mt-5 text-white bg-red-500 rounded-lg transition ease-in-out hover:bg-gray-900">
+      <div className="flex flex-col items-start justify-center text-xl font-semibold px-4 pl-3 pr-20 py-4 mt-5 text-white bg-red-500 rounded-lg transition ease-in-out hover:bg-gray-900">
         <h3>
         <span className="px-2">
           <DiscordLogo />
