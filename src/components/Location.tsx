c43ab2706d84ce887aaab4f7f953be16dc401da4
kaicoleridge <kai@coleridge.me>
2023-02-22 19:13:21 +0000

    commit

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -6,8 +6,6 @@ const Location = () => {
   const { data: user } = useLanyard(DISCORD_ID);
 
   return (
-    // show location kv pair
-
     <div className="flex flex-col items-start justify-start text-sm font-semibold px-5 py-4 mt-5 dark:bg-[#12181d]/60 rounded-lg p-3 transition ease-in-out hover:bg-gray-900">
       <a href={user?.kv.googleMapURL} target="_blank" rel="noreferrer">
         <p className="text-lg text-white">📍 {user?.kv.location}</p>
