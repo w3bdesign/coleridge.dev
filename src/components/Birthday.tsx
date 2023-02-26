2b79b436c9c9b00892d4a43c00606997720f3fa2
kaicoleridge <kai@coleridge.me>
2023-02-26 23:51:08 +0000

    rollback

--- src/components/Birthday.tsx
+++ src/components/Birthday.tsx
@@ -1,5 +1,5 @@
 const birthday = () => {
-  const birthday = new Date("2023-03-30 GMT");
+  const birthday = new Date(2023, 2, 30);
   const today = new Date()
   const diff = birthday.getTime() - today.getTime();
   const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
