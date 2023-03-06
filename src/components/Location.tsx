6ea4f001593ad7405fd86e820c3e53fccd912e6e
kaicoleridge <kai@coleridge.me>
2023-03-06 22:46:19 +0000

    feat: added tooltip to location component

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,4 +1,6 @@
 import { useLanyard } from "use-lanyard";
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import "react-tooltip/dist/react-tooltip.css";
 
 const DISCORD_ID = "328282806327181322";
 
@@ -7,9 +9,10 @@ const Location = () => {
 
   return (
     <div className="flex flex-col items-start justify-start text-sm font-semibold px-5 py-4 mt-5 dark:bg-[#12181d]/60 rounded-lg p-3 transition ease-in-out hover:bg-gray-900">
-      <a href={user?.kv.googleMapURL} target="_blank" rel="noreferrer">
-        <p className="text-lg text-white">📍 {user?.kv.location}</p>
+      <a href={user?.kv.googleMapURL}  target="_blank" rel="noreferrer" id="location" data-tooltip-content="Current Location">
+        <p className="text-lg text-white" >📍 {user?.kv.location}</p>
       </a>
+      <ReactTooltip anchorId="location" />
     </div>
   );
 };
